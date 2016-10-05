7562898bb88ac3357989e029071a3b41952252d3
luhtonen <luhtonen@gmail.com>
2016-10-05 08:40:36 +0200

    Saving user to database

--- chapter01/ch01_hello.go
+++ chapter01/ch01_hello.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
+
+var database *sql.DB
 
 // API is Rest API struct
 type API struct {
 	Message string `json:"message"`
+}
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
+	First string `json:"first"`
+	Last  string `json:"last"`
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
@@ -21,15 +34,46 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	output, err := json.Marshal(message)
 
 	if err != nil {
-		fmt.Println("Something went wrong!")
+		fmt.Println("Something went wrong!", err.Error())
 	}
 
 	fmt.Fprintf(w, string(output))
 }
 
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	newUser := User{}
+	newUser.Name = r.FormValue("user")
+	newUser.Email = r.FormValue("email")
+	newUser.First = r.FormValue("first")
+	newUser.Last = r.FormValue("last")
+	output, err := json.Marshal(newUser)
+	fmt.Println(string(output))
+	if err != nil {
+		fmt.Println("Something went wrong", err.Error())
+	}
+
+	query := "INSERT INTO users set user_nickname='" + newUser.Name +
+		"', user_first='" + newUser.First +
+		"', user_last='" + newUser.Last + "', user_email='" + newUser.Email + "'"
+
+	q, err := database.Exec(query)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(q)
+}
+
 func main() {
-	gorillaRoute := mux.NewRouter()
-	gorillaRoute.HandleFunc("/api/{user:[0-9]+}", Hello)
-	http.Handle("/", gorillaRoute)
+	db, err := sql.Open("mysql", "gosocnet:gosocnet@/social_network")
+	if err != nil {
+		fmt.Println("cannot connec to database", err.Error())
+		os.Exit(1)
+	}
+	database = db
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/api/{user:[0-9]+}", Hello)
+	routes.HandleFunc("/api/user/create", CreateUser).Methods("GET")
+	http.Handle("/", routes)
 	http.ListenAndServe(":8080", nil)
 }
