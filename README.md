8345b5b4fcf501b0293d5125c26ace7eb8544b53
luhtonen <luhtonen@gmail.com>
2016-10-07 09:01:10 +0200

    Converting to RESTful

---
+++ README.md
@@ -0,0 +1,53 @@
+# Mastering Go Web Services
+
+## Setup MySQL
+
+Start MySQL console as root: `mysql -u root` and create database:
+
+```
+create database social_network;
+``` 
+
+Then create database user and grant access for this user to the newly
+created database:
+
+```
+grant
+```
+
+Create `users` table:
+
+```
+CREATE TABLE users (
+    user_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_nickname VARCHAR(32) NOT NULL,
+    user_first VARCHAR(32) NOT NULL,
+    user_last VARCHAR(32) NOT NULL,
+    user_email VARCHAR(128) NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE INDEX user_nickname (user_nickname)
+);
+```
+
+Add new unique index to `users` table:
+
+```
+ALTER TABLE users ADD UNIQUE INDEX user_email (user_email); 
+```
+
+Create new `users_relationships` table:
+
+```
+CREATE TABLE users_relationships (
+    users_relationship_id INT(13) NOT NULL AUTO_INCREMEMT,
+    from_user_id INT(10) NOT NULL,
+    to_user_id INT(10) UNSIGNED NOT NULL,
+    users_relationship_type VARCHAR(10) NOT NULL,
+    users_relationship_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (users_relationship_id),
+    INDEX from_user_id (from_user_id),
+    INDEX to_user_id (to_user_id),
+    INDEX from_user_id_to_user_id (from_user_id, to_user_id),
+    INDEX from_user_id_to_user_id_users_relationship_type (from_user_id, to_user_id, users_relationship_type)
+);
+```
---
+++ chapter02/ch02_crud.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+var database *sql.DB
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
+	First string `json:"first"`
+	Last  string `json:"last"`
+}
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+	newUser := User{}
+	newUser.Name = r.FormValue("user")
+	newUser.Email = r.FormValue("email")
+	newUser.First = r.FormValue("first")
+	newUser.Last = r.FormValue("last")
+	output, err := json.Marshal(newUser)
+	fmt.Println(string(output))
+	if err != nil {
+		fmt.Println("Something went wrong", err.Error())
+	}
+
+	query := "INSERT INTO users set user_nickname='" + newUser.Name +
+		"', user_first='" + newUser.First +
+		"', user_last='" + newUser.Last + "', user_email='" + newUser.Email + "'"
+
+	q, err := database.Exec(query)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(q)
+}
+
+func UsersRetrieve(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Pragma", "no-cache")
+	rows, _ := database.Query("select * from users LIMIT 10")
+	response := Users{}
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.ID, &user.Name, &user.First, &user.Last, &user.Email)
+		response.Users = append(response.Users, user)
+	}
+	output, _ := json.Marshal(response)
+	fmt.Fprint(w, string(output))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Pragma", "no-cache")
+	urlParams := mux.Vars(r)
+	id := urlParams["id"]
+	readUser := User{}
+	err := database.QueryRow("select * from users where user_id=?", id).
+		Scan(&readUser.ID, &readUser.Name, &readUser.First, &readUser.Last, &readUser.Email)
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Fprint(w, "No such user")
+	case err != nil:
+		log.Fatal(err.Error())
+	default:
+		output, _ := json.Marshal(readUser)
+		fmt.Fprintf(w, string(output))
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "gosocnet:gosocnet@/social_network")
+	if err != nil {
+		fmt.Println("cannot connec to database", err.Error())
+		os.Exit(1)
+	}
+	database = db
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/api/users", UserCreate).Methods("POST")
+	routes.HandleFunc("/api/users", UsersRetrieve).Methods("GET")
+	routes.HandleFunc("/api/user/{id:[0-9]+}", GetUser).Methods("GET")
+	http.Handle("/", routes)
+	http.ListenAndServe(":8080", nil)
+}
