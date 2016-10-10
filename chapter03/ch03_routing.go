517867de13a4f673fe9cdac0da376b951963ad36
luhtonen <luhtonen@gmail.com>
2016-10-10 10:13:51 +0200

    Responding with error

--- chapter03/ch03_routing.go
+++ chapter03/ch03_routing.go
@@ -1,13 +1,101 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc"
-	"github.com/gorilla/rpc/json"
+	rjson "github.com/gorilla/rpc/json"
+	"log"
 	"net/http"
-	"unicode/utf8"
+	"os"
 	"strconv"
+	"unicode/utf8"
 )
+
+var database *sql.DB
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
+	First string `json:"first"`
+	Last  string `json:"last"`
+}
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type CreateResponse struct {
+	Error string `json:"error"`
+}
+
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+	newUser := User{}
+	newUser.Name = r.FormValue("user")
+	newUser.Email = r.FormValue("email")
+	newUser.First = r.FormValue("first")
+	newUser.Last = r.FormValue("last")
+	output, err := json.Marshal(newUser)
+	fmt.Println("output:", string(output))
+	if err != nil {
+		fmt.Println("Something went wrong", err.Error())
+	}
+
+	response := CreateResponse{}
+	query := "INSERT INTO users set user_nickname='" + newUser.Name +
+		"', user_first='" + newUser.First +
+		"', user_last='" + newUser.Last + "', user_email='" + newUser.Email + "'"
+
+	q, err := database.Exec(query)
+	if err != nil {
+		response.Error = err.Error()
+	}
+	fmt.Println(q)
+	createOutput, _ := json.Marshal(response)
+	fmt.Fprintln(w, string(createOutput))
+}
+
+func UsersRetrieve(w http.ResponseWriter, r *http.Request) {
+	start := 0
+	limit := 10
+	next := start + limit
+
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Link", "<http://localhost:8080/api/users?start="+string(next)+
+		"; rel=\"next\"")
+
+	rows, _ := database.Query("select * from users LIMIT ?", limit)
+	response := Users{}
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.ID, &user.Name, &user.First, &user.Last, &user.Email)
+		response.Users = append(response.Users, user)
+	}
+	output, _ := json.Marshal(response)
+	fmt.Fprint(w, string(output))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Pragma", "no-cache")
+	urlParams := mux.Vars(r)
+	id := urlParams["id"]
+	readUser := User{}
+	err := database.QueryRow("select * from users where user_id=?", id).
+		Scan(&readUser.ID, &readUser.Name, &readUser.First, &readUser.Last, &readUser.Email)
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Fprint(w, "No such user")
+	case err != nil:
+		log.Fatal(err.Error())
+	default:
+		output, _ := json.Marshal(readUser)
+		fmt.Fprintf(w, string(output))
+	}
+}
 
 type RPCAPIArguments struct {
 	Message string
@@ -29,14 +117,32 @@ func (h *StringService) Length(r *http.Request, arguments *RPCAPIArguments, repl
 	return nil
 }
 
-// test with following command:
-// curl -X POST -H "Content-Type: application/json" -d '{"method":"StringService.Length","params":[{"Message":"Testing the service, how long is it?"}], "id":"1"}' http://localhost:10000/rpc
+/* rpc test with following command:
+curl -X POST -H "Content-Type: application/json" -d '{"method":"StringService.Length","params":[{"Message":"Testing the service, how long is it?"}], "id":"1"}' http://localhost:10000/rpc
+*/
 
+/* error test:
+curl -X POST -d '{name=edu&email=edufinn&first=Edu&last=Finn}' http://localhost:8080/api/users
+*/
 func main() {
+	db, err := sql.Open("mysql", "gosocnet:gosocnet@/social_network")
+	if err != nil {
+		fmt.Println("cannot connec to database", err.Error())
+		os.Exit(1)
+	}
+	database = db
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/api/users", UserCreate).Methods("POST")
+	routes.HandleFunc("/api/users", UsersRetrieve).Methods("GET")
+	routes.HandleFunc("/api/user/{id:[0-9]+}", GetUser).Methods("GET")
+	http.Handle("/", routes)
+
 	fmt.Println("Starting service...")
 	s := rpc.NewServer()
-	s.RegisterCodec(json.NewCodec(), "application/json")
+	s.RegisterCodec(rjson.NewCodec(), "application/json")
 	s.RegisterService(new(StringService), "")
 	http.Handle("/rpc", s)
-	http.ListenAndServe(":10000", nil)
+	//http.ListenAndServe(":10000", nil)
+	http.ListenAndServe(":8080", nil)
 }
