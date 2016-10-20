e62f100bc7ba36b64e898620e07b061ef05b8ed4
luhtonen <luhtonen@gmail.com>
2016-10-20 19:50:00 +0200

    Example of password hashing

--- chapter05/ch05_templates.go
+++ chapter05/ch05_templates.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 )
 
 const (
@@ -25,7 +29,43 @@ func redirectNonSecure(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
 }
 
+const randomLength = 45
+
+func GenerateSalt(length int) string {
+	var salt []byte
+	var asciiPad int64
+
+	if length == 0 {
+		length = randomLength
+	}
+
+	asciiPad = 32
+
+	for i := 0; i < length; i++ {
+		salt = append(salt, byte(rand.Int63n(94)+asciiPad))
+	}
+	return string(salt)
+}
+
+func GenerateHash(salt string, password string) string {
+	var hash string
+	fullString := salt + password
+	sha := sha256.New()
+	sha.Write([]byte(fullString))
+	hash = base64.URLEncoding.EncodeToString(sha.Sum(nil))
+	return hash
+}
+
+func ReturnPassword(password string) (string, string) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	salt := GenerateSalt(0)
+	hash := GenerateHash(salt, password)
+	return salt, hash
+}
+
 func main() {
+	salt, hash := ReturnPassword("password")
+	fmt.Println("password salt and hash:", salt, hash)
 	wg := sync.WaitGroup{}
 	log.Println("Starting redirection server, try to access @ http:")
 
