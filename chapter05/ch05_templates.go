f50a693f621b3655ad1bb7b75f2153935d3df5d8
luhtonen <luhtonen@gmail.com>
2016-10-20 19:25:47 +0200

    Implementing TLS security NOTE: that port under 1024 cannot be used by user processes, this is why port 1443 was used as SSL port

--- chapter05/ch05_templates.go
+++ chapter05/ch05_templates.go
@@ -9,20 +9,20 @@ import (
 
 const (
 	serverName   = "localhost"
-	SSLport      = ":443"
+	SSLport      = ":1443"
 	HTTPport     = ":8080"
 	SSLprotocol  = "https://"
 	HTTPprotocol = "http://"
 )
 
 func secureRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "You have arrived at port 443, but you are not yet secure.")
+	fmt.Fprintln(w, "You have arrived at port 443, and now you are marginally more secure.")
 }
 
 func redirectNonSecure(w http.ResponseWriter, r *http.Request) {
 	log.Println("Non-secure request initiated, redirecting.")
-	redirectUrl := SSLprotocol + serverName + r.RequestURI
-	http.Redirect(w, r, redirectUrl, http.StatusOK)
+	redirectURL := SSLprotocol + serverName + SSLport + r.RequestURI
+	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
 }
 
 func main() {
@@ -36,7 +36,7 @@ func main() {
 	}()
 	wg.Add(1)
 	go func() {
-		http.ListenAndServe(SSLport, http.HandlerFunc(secureRequest))
+		http.ListenAndServeTLS(SSLport, "server.pem", "server.key", http.HandlerFunc(secureRequest))
 		wg.Done()
 	}()
 	wg.Wait()
